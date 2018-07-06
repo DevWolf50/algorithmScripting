37f6d455e2e68231822c7481acd90c0240498752
joepal <joe.palomino50@gmail.com>
2018-07-05 22:40:56 -0600

    progress

--- basicAlgorithmScripting.js
+++ basicAlgorithmScripting.js
@@ -1,5 +1,30 @@
-const cToF = (celsius) => celsius * (9/5) + 32;
+const cToF = celsius => celsius * (9 / 5) + 32;
 
-const reverseString = (str) => [...str].reverse().join('');
+const reverseString = str => [...str].reverse().join('');
 
-const factorialize = (num) => num == 0 ? 1 : num * factorialize(num - 1);
+const factorialize = num => (num == 0 ? 1 : num * factorialize(num - 1));
+
+const findLongestWordLength = str =>
+  str
+    .split(/\s/)
+    .reduce((acc, elem) => (elem.length > acc ? elem.length : acc), 0);
+
+const largestOfFour = arr =>
+  arr.reduce(
+    (acc, elem) => [
+      ...acc,
+      elem.reduce((acc, elem) => (elem > acc ? elem : acc))
+    ],
+    []
+  );
+
+const confirmEnding = (str = '', endMatch = '') => str.length > endMatch.length && str.substr(str.length - endMatch.length) === endMatch;
+
+const repeatStringNumTimes = (str = '', num = 0) => {
+  let newStr = '';
+  for(let i = 0; i < num; i++) {
+    newStr += str;
+  }
+
+  return newStr;
+}
