df9480f2b80ede3977630f86cff524026591bf02
joepal <joe.palomino50@gmail.com>
2018-07-10 16:28:44 -0600

    add 4 more algorithms

--- intermediateAlgorithms.js
+++ intermediateAlgorithms.js
@@ -4,3 +4,44 @@ const sumAll = (start, end) =>
   start > end ? 0 : start + sumAll(start + 1, end);
 
 const sum = nums => sumAll(...nums.sort((a, b) => a - b));
+
+//return the symmetric difference between two arrays
+const diffArray = (arr1, arr2) => {
+  //iterate through the first array
+  //if an element is not in the 2nd array store it
+  const diff1 = arr1.filter(elem => !arr2.includes(elem));
+
+  //iterate through the 2nd array
+  //if an element is not in the 1st array store it
+  const diff2 = arr2.filter(elem => !arr1.includes(elem));
+
+  //return both arrays combinedP
+  return [...diff1, ...diff2];
+};
+
+const diffArray2 = (arr1, arr2) =>
+  //combine both arrays
+  [...arr1, ...arr2].filter(
+    (elem, index, arr) =>
+      //find only elements that don't exists in the rest of the array and is the first occurrence in the array
+      !arr.slice(index + 1).includes(elem) && arr.indexOf(elem) === index
+  );
+
+//will remove elements from an array(first argument) that match the rest of the arguments
+const destroyer = (arr, ...destroy) =>
+  arr.filter(elem => !destroy.includes(elem));
+
+//will return an array of all objects that contain a specific prop and value pair
+const whatIsInAName = (arr, testObj) =>
+  arr.filter(elem =>
+    Object.keys(testObj).every(key => elem[key] == testObj[key])
+  );
+
+//converts a string to spinal case
+const spinalCase = str =>
+  str
+    .replace(
+      /[\s_]|[a-z](?=[A-Z])/g,
+      str => (/[a-z]/.test(str) ? `${str}-` : '-')
+    )
+    .toLowerCase();
